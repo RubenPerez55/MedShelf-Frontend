b398bb9083c6db1253f2195dba13c313d4cc4de1
HectorBarrera13 <hectoralejandro.bc@gmail.com>
2026-03-27 12:18:34 -0600

    Meds styles pending

---
+++ MedShelf/src/app/features/meds/meds.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meds',
+  imports: [],
+  templateUrl: './meds.html',
+  styleUrl: './meds.css',
+})
+export class Meds {
+
+}

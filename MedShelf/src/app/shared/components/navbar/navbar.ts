3b9dfbfc08f5d3c952a14608317e8eb124994413
HectorBarrera13 <hectoralejandro.bc@gmail.com>
2026-03-27 11:29:11 -0600

    Nav bar styles pending

--- MedShelf/src/app/shared/components/navbar/navbar.ts
+++ MedShelf/src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LucideAngularModule, House, BriefcaseMedical, Pill, User } from 'lucide-angular';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, LucideAngularModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
-
+  icons = {
+    home: House,
+    dashboard: BriefcaseMedical,
+    meds: Pill,
+    profile: User,
+  };
 }
